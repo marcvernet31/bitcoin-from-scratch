257e0c824c168e71d701a634a32e95f6f895d684
marcvernet31 <44305244+marcvernet31@users.noreply.github.com>
2021-07-05 17:58:22 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+
+
+# Reward given to block creator in Coinbase transaction
+reward = 100
+
+# No. of bytes selected for wallet adress checksum
+checksum_length = 4
+
+# version (adress generation)
+version = bytes(0x00) # ?? wtf
+
+# ProofOfWork bits of difficulty
+difficulty = 12
+
+# Mixumum nonce allowed in ProofOfWork
+# 4 billion (same as Bitcoin)
+max_nonce = 2 ** 32
